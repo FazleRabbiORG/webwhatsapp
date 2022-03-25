efea9706e32e8374a76650fd0f04d9ca0ac557f3
Fazle Rabbi <24.fazlerabbi@gmail.com>
2022-03-26 02:41:08 +0600

    whatsapp web

---
+++ index.js
@@ -0,0 +1,45 @@
+const qrcode = require("qrcode-terminal");
+const fs = require("fs");
+const { Client, LegacySessionAuth } = require("whatsapp-web.js");
+
+const SESSION_FILE_PATH = "./session.json";
+
+// Load the session data if it has been previously saved
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionData = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  authStrategy: new LegacySessionAuth({
+    session: sessionData,
+  }),
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("authenticated", (session) => {
+  sessionData = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+    if (err) {
+      console.error("error", err);
+    }
+  });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+  client.getChats().then((chats) => {
+    myConversation = chats.find((chat) => chat.id.user === "8801796127162");
+    for (let i = 0; i < 50; i++) {
+      setTimeout(() => {
+        client.sendMessage(myConversation.id._serialized, " hello ");
+      }, i*5000);
+    }
+    console.log(myConversation);
+  });
+});
+
+client.initialize();
